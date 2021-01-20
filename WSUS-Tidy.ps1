5154c0af34194dc8edbaebfb6a0299762a0da308
Peter Marquis <EnviableOne@users.noreply.github.com>
2021-01-20 12:47:12 +0000

    Update WSUS-Tidy.ps1
    Tweaked Output for clarity
    removed unused variables
    renamed unclear variables
    fixed count output

--- WSUS-Tidy.ps1
+++ WSUS-Tidy.ps1
@@ -1,6 +1,7 @@
-﻿[void][reflection.assembly]::LoadWithPartialName("Microsoft.UpdateServices.Administration")
+[cmdletbinding()]
+Param()
+[void][reflection.assembly]::LoadWithPartialName("Microsoft.UpdateServices.Administration")
 
-$firstdayofmonth = [datetime] ([string](get-date).AddMonths(-1).month + "/1/" + [string](get-date).year)
 $DomainName = "."+ $env:USERDNSDOMAIN
 $dateText = (Get-Date).ToString('MM-dd-yyyy_hh-mm-ss')
 
@@ -8,11 +9,11 @@ $dateText = (Get-Date).ToString('MM-dd-yyyy_hh-mm-ss')
 $UpdateStatus = @()
 $SummaryStatus = @()		
 
-# For WSUS servers catering servers
-$WSUSServers = ("wsus-pcs","wsus01-com","wsus-servers","EMIS-SPOKE-3","EMIS-SPOKE-4","EMIS-SPOKE-5","EMIS-SPOKE-6","EMIS-SPOKE-7","EMIS-SPOKE-8","EMIS-SPOKE-9")
+# WSUS servers in environment
+$WSUSServers = ("wsus-pcs","wsus-com","wsus-servers","WSUS-3","WSUS-4","WSUS-5","WSUS-6","WSUS-7","WSUS-8","WSUS-9")
 
-$a0 = ($WSUSServers | measure).count
-$b0 = 0
+$ServerCount = ($WSUSServers | measure).count
+$CurrentServer = 0
 
 $thisDir = try{ Split-Path -Parent $MyInvocation.MyCommand.Path} Catch {"C:\wsus-reports\transcripts\"}
 $logFile = "WSUSDeadCleanUP_$dateText.txt"
@@ -20,11 +21,10 @@ $allComp = (Get-ADComputer -Filter *) + (Get-ADComputer -Filter * -SearchBase "D
 $allCompCount = ($allComp).count
 Start-Transcript -Path $thisDir\$logFile
 
-
 ForEach ($WSUSServer in $WSUSServers) {		
-        write-host "Connecting to $WSUSServer ..."	-foregroundcolor Green
-        $b0 = $b0+1
-		
+        $CurrentServer++
+        write-host "Connecting to $WSUSServer ( Server $CurrentServer of $ServerCount) ..."	-foregroundcolor Green
+	
     try {
         
 		Try {
@@ -43,15 +43,15 @@ ForEach ($WSUSServer in $WSUSServers) {
         $deadcompcount = ($deadcomp).count
         Write-Verbose "done."
         Write-verbose "Macines in AD = $AllCompCount, Machines in $WSUSServer = $WSUSMCsCount, Machines in $WSUSServer not in AD = $DeadcompCount"
-        if ($Deadcomp.count  -ne 0) {
-         write-host "$($Deadcomp.count) computers are Not Active" -ForegroundColor Red
+        if ($DeadcompCount  -ne 0) {
+         write-host "$DeadcompCount computers are Not Active" -ForegroundColor Red
          Write-host $Deadcomp.FullDomainName -Separator "`n`r" -ForegroundColor white
          $del = Read-Host -prompt "Do You Wish to Delete ALL wsus hosts not in AD [Y/n]"
          if (($del -eq "Y") -or($del -eq "Y*") -or ($AutoDelete)){
-          write-Verbose "Deleting $deadcount Machines from $wsusserver ..."
+          write-Verbose "Deleting $DeadcompCount Machines from $wsusserver ..."
           $Deadcomp.delete()
           Write-verbose "done."
-          write-Host "$deadcount Machines removed from $wsusserver"
+          write-Host "$DeadcompCount Machines removed from $wsusserver"
          }
         }
         Else {
